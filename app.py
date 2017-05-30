0c8fcfc9076090203e3b51efc0632af0986435ca
Andrey Biryukov <biryukov.andrey@gmail.com>
2017-05-30 23:18:48 +0300

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+from aiohttp import web
+
+app = web.Application()
+
+
+async def hello_post(request):
+    return web.Response(text='POST request')
+
+
+async def hello_get(request):
+    return web.Response(text='GET request')
+
+
+app.router.add_post('/hello', hello_post)
+app.router.add_get('/hello', hello_get)
+
+
+web.run_app(app)
